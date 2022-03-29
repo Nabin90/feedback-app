c7dc0f53230a2f288536aba06b2e1a71fce72a78
Nabin90 <nabinpunis90@gmail.com>
2022-03-29 11:49:50 +0545

    added mock backend

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,34 +1,35 @@
 import { motion, AnimatePresence } from 'framer-motion';
-import PropTypes from 'prop-types';
+import { useContext } from 'react';
 import FeedbackItem from './FeedbackItem';
+import Spinner from './shared/Spinner';
+import FeedbackContext from '../context/FeedbackContext';
 
-function FeedbackList({ feedback, handleDelete }) {
+function FeedbackList() {
   
-  if (!feedback || feedback.length === 0) {
+  const {feedback, isLoading} = useContext(FeedbackContext);
+
+  if (!isLoading && (!feedback || feedback.length === 0)) {
       return <p>No Feedback Yet!</p>
   }
 
-  return (
-    <div className='feedback-list'>
-        <AnimatePresence>
-            {feedback.map((item) => (
-                <motion.div
-                    key={item.id}
-                    initial={{opacity: 0}}
-                    animate={{opacity: 1}}
-                    exit={{opacity: 0}}
-                >
-                    <FeedbackItem 
-                        key={item.id} 
-                        item={item} 
-                        handleDelete={handleDelete }
-                    />
-                </motion.div>
-            ))}
-        </AnimatePresence>
-    </div>
-  )
-    
+  return isLoading ? <Spinner /> : (<div className='feedback-list'>
+  <AnimatePresence>
+      {feedback.map((item) => (
+          <motion.div
+              key={item.id}
+              initial={{opacity: 0}}
+              animate={{opacity: 1}}
+              exit={{opacity: 0}}
+          >
+              <FeedbackItem 
+                  key={item.id} 
+                  item={item} 
+              />
+          </motion.div>
+      ))}
+  </AnimatePresence>
+</div>);
+  
 //   return (
 //     <div className='feedback-list'>
 //         {feedback.map((item) => (
@@ -40,16 +41,6 @@ function FeedbackList({ feedback, handleDelete }) {
 //         ))}
 //     </div>
 //   )
-}
-
-FeedbackList.propTypes = {
-    feedback: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            text: PropTypes.string.isRequired,
-            rating: PropTypes.number.isRequired,
-        })
-    )
 }
 
 export default FeedbackList;
